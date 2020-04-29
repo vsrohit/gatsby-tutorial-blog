86ef674357e7daf94ee352901b2d859deab7f416
Rohit <vsrohit@users.noreply.github.com>
2020-04-29 21:44:10 +0530

    addig files directly from public

---
+++ component---src-pages-about-js-8314849a2f211efc951b.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{"3XHS":function(t,e,n){"use strict";n.r(e),n.d(e,"query",(function(){return u}));n("q1tI");var o=n("Bl7J"),a=n("qKvR");e.default=function(t){var e=t.data;return Object(a.b)(o.a,null,Object(a.b)("h1",null,"About ",e.site.siteMetadata.title),Object(a.b)("p",null,"We're the only site running on your computer dedicated to showing the best photos and videos of pandas eating lots of food."))};var u="1097489062"}}]);
+//# sourceMappingURL=component---src-pages-about-js-8314849a2f211efc951b.js.map
---
+++ component---src-templates-blog-post-js-388cfbb7071cd224c813.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{yZlL:function(t,n,e){"use strict";e.r(n),e.d(n,"query",(function(){return l}));e("q1tI");var r=e("Bl7J"),a=e("qKvR");n.default=function(t){var n=t.data.markdownRemark;return Object(a.b)(r.a,null,Object(a.b)("div",null,Object(a.b)("h1",null,n.frontmatter.title),Object(a.b)("div",{dangerouslySetInnerHTML:{__html:n.html}})))};var l="861893041"}}]);
+//# sourceMappingURL=component---src-templates-blog-post-js-388cfbb7071cd224c813.js.map
